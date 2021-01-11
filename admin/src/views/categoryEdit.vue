47b9ca2091dc1605ec9c98f1f809c3259bfb7869
qulingfeng <1149446275@qq.com>
2021-01-11 10:54:53 +0800

    init

---
+++ admin/src/views/categoryEdit.vue
@@ -0,0 +1,18 @@
+<template>
+    <div >
+        <h1>新建分类</h1>
+        <el-form>
+            <el-form-item label="名称">
+                <el-input></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary">保存</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+
+</template>
+
+<script>
+
+</script>
